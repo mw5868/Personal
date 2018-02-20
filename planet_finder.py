91771e4b5bef21e30625cdd82756506c2e96a7d3
mw5868 <30810584+mw5868@users.noreply.github.com>
2018-02-20 19:49:23 +0000

    new exoplanet finder

--- planet_finder.py
+++ planet_finder.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Feb 17 17:54:38 2018
+Created on Tue Feb 20 19:30:46 2018
 
 @author: mwood
 """
+
 import numpy as np
 import scipy.signal
 import matplotlib.pyplot as plt
@@ -14,12 +15,14 @@ from matplotlib import rcParams
 rcParams["figure.figsize"] = (14, 5)
 rcParams["font.size"] = 20
 
-url = "https://archive.stsci.edu/missions/kepler/target_pixel_files/0119/011904151/kplr011904151-2010009091648_lpd-targ.fits.gz"
-url2 = "https://archive.stsci.edu/missions/kepler/target_pixel_files/0019/001995038/kplr001995038-2009131105131_lpd-targ.fits.gz"
+#url = "https://archive.stsci.edu/missions/kepler/target_pixel_files/0119/011904151/kplr011904151-2010009091648_lpd-targ.fits.gz"
+#url2 = "https://archive.stsci.edu/missions/kepler/target_pixel_files/0019/001995038/kplr001995038-2009131105131_lpd-targ.fits.gz"
+print("You find the URL via STSCI.edu website")
+url = str(input("Enter URL for FITS: "))
 
 # Read in Kepler data for star number 011904151
 import astropy.io.fits
-data = astropy.io.fits.open(url2)[1].data
+data = astropy.io.fits.open(url)[1].data
 time = data["TIME"][data['QUALITY'] == 0]
 images = data["FLUX"][data['QUALITY'] == 0]
 
@@ -28,22 +31,22 @@ time[0:5]  # The units are in days
 images[0:5]  # The images give us photons per pixel per second
 
 # Let's plot the image at the first timestamp
-plt.imshow(images[0], cmap='gray', interpolation='nearest');
+#plt.imshow(images[0], cmap='gray', interpolation='nearest');
 
 # Let's create a lightcurve by summing the flux in all the time cadences
 lightcurve = np.sum(images, axis=(1, 2))
-
+'''
 plt.plot(time, lightcurve, '.')
 plt.ylabel("Brightness")
 plt.xlabel("Time");
-
+'''
 trend = scipy.signal.savgol_filter(lightcurve, 101, polyorder=3) 
 percent_change = 100 * ((lightcurve / trend) - 1)
 
-plt.plot(time, percent_change, '.')
+'''plt.plot(time, percent_change, '.')
 plt.ylabel("Brightness change (%)")
 plt.xlabel("Time");
-
+'''
 # We will use the Lomb-Scargle Periodogram.
 # For background, see Jake VanderPlas' blog at https://jakevdp.github.io/blog/2015/06/13/lomb-scargle-in-python/
 from astropy.stats import LombScargle
@@ -54,16 +57,17 @@ plt.plot(1 / frequency, power)
 plt.xlabel("Period (days)")
 plt.ylabel("Power");
 
+
 n_plots = 10
+'''
 plt.figure(figsize=(10, 30))
 for i in range(n_plots):
     mask = (time >= time[0] + i*period) & (time < time[0] + (i+1)*period)
     plt.subplot(n_plots, 1, i+1)
     plt.scatter(time[mask], percent_change[mask], c='C{}'.format(i))
-
+'''
 plt.figure(figsize=(10, 5))
 for i in range(n_plots):
     mask = (time >= time[0] + i*period) & (time < time[0] + (i+1)*period)
     plt.scatter(time[mask] - time[0] - i*period, percent_change[mask])
-
-
+plt.show()
